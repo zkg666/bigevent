f303a265903775f3cc425cb5c597a9cd71b6345c
zkg666 <90459567@qq.com>
2019-09-21 16:43:00 +0800

    管理员退出功能完成

--- web_back/js/utils/common.js
+++ web_back/js/utils/common.js
@@ -1,6 +1,6 @@
 // 定义一个对象存储的是封装的功能
 var user = {
-    //第一ajax请求功能，判断登录名密码正确否
+    //第一个ajax请求功能，判断登录名密码是否正确的功能
     login:function(userName,password,callback){
         //提交ajax请求
         $.ajax({
@@ -11,5 +11,17 @@ var user = {
               callback(res);
             }
         })
-    }   
+    },   
+    //第二个ajax请求功能，进行管理员退出功能
+    logout:function(callback){
+        $.ajax({
+            url:'http://localhost:8000/admin/logout',
+            type:'post',
+            success:function(res){
+                callback(res);
+            }
+        })
+    },
+    //第三个ajax请求功能，获取用户信息的功能
+  
 }
